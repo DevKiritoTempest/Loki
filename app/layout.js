bc9a2fe683aff014636aa3c03f2ca220d2e84590
DevKiritoTempest <ralyson156@gmail.com>
2025-11-16 10:42:57 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: "Projeto Loki",
+  description: "Sistema multi-agentes Aurix",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body style={{ fontFamily: "sans-serif", padding: 20 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
